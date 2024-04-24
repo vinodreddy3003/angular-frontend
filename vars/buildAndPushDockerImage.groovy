628ad0f97cf6a354175ade6f4fe203b286e5c101
Devendra-Reddy-420 <136948121+Devendra-Reddy-420@users.noreply.github.com>
2024-04-24 20:17:00 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -2,28 +2,28 @@ def call(String imageName, String awsAccountId, String awsRegion) {
     pipeline {
         agent any
       
-         stages {
-        //     stage('Authenticate with AWS ECR') {
-        //         steps {
-        //             script {
-        //                 withCredentials([[
-        //                     $class: 'AmazonWebServicesCredentialsBinding',
-        //                     accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-        //                     credentialsId: 'Amazon Credentials',
-        //                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-        //                 ]]) {
-        //                     sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com"
-        //                 }
-        //             }
-        //         }
-        //     }
-             stage('Authenticate with AWS ECR'){
-               steps{
-                 script{
-                   sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com"
-                 }
-               }
+          stages {
+             stage('Authenticate with AWS ECR') {
+                 steps {
+                     script {
+                         withCredentials([[
+                             $class: 'AmazonWebServicesCredentialsBinding',
+                             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                             credentialsId: 'K8s',
+                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                         ]]) {
+                            bat 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com'
+                        }
+                    }
+                }
              }
+             // stage('Authenticate with AWS ECR'){
+             //   steps{
+             //     script{
+             //       sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com"
+             //     }
+             //   }
+             // }
              stage('Build Docker Image') {
                 steps {
                     script {
