862688f942fa4743eff11cb7cab1ea68639f4c66
Devendra-Reddy-420 <136948121+Devendra-Reddy-420@users.noreply.github.com>
2024-04-24 19:32:39 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -3,6 +3,20 @@ def call(String imageName, String awsAccountId, String awsRegion) {
         agent any
 
         stages {
+            stage('Authenticate with AWS ECR') {
+                steps {
+                    script {
+                        withCredentials([[
+                            $class: 'AmazonWebServicesCredentialsBinding',
+                            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                            credentialsId: 'K8s',
+                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                        ]]) {
+                            sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com"
+                        }
+                    }
+                }
+            }
             stage('Build Docker Image') {
                 steps {
                     script {
@@ -13,7 +27,7 @@ def call(String imageName, String awsAccountId, String awsRegion) {
             stage('Push Docker Image to AWS ECR') {
                 steps {
                     script {
-                        docker.withRegistry('https://891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-frontend') {
+                        docker.withRegistry('https://891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app') {
                             docker.image('angular-app').push('latest')
                         }
                     }
