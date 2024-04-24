7104d862cfe4b748a0b6e776cab6d6cd1a0d6700
Devendra-Reddy-420 <136948121+Devendra-Reddy-420@users.noreply.github.com>
2024-04-24 19:56:18 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -1,23 +1,23 @@
 def call(String imageName, String awsAccountId, String awsRegion) {
     pipeline {
         agent any
-
-        stages {
-            stage('Authenticate with AWS ECR') {
-                steps {
-                    script {
-                        withCredentials([[
-                            $class: 'AmazonWebServicesCredentialsBinding',
-                            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                            credentialsId: 'Amazon Credentials',
-                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                        ]]) {
-                            sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com"
-                        }
-                    }
-                }
-            }
-            stage('Build Docker Image') {
+      
+        // stages {
+        //     stage('Authenticate with AWS ECR') {
+        //         steps {
+        //             script {
+        //                 withCredentials([[
+        //                     $class: 'AmazonWebServicesCredentialsBinding',
+        //                     accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+        //                     credentialsId: 'Amazon Credentials',
+        //                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+        //                 ]]) {
+        //                     sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com"
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     stage('Build Docker Image') {
                 steps {
                     script {
                         docker.build('angular-app')
