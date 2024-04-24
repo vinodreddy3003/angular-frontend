973a0a4418667359e1688a4e89add095fd59882a
Devendra-Reddy-420 <136948121+Devendra-Reddy-420@users.noreply.github.com>
2024-04-24 19:50:41 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -9,7 +9,7 @@ def call(String imageName, String awsAccountId, String awsRegion) {
                         withCredentials([[
                             $class: 'AmazonWebServicesCredentialsBinding',
                             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                            credentialsId: 'AWS-Credentials',
+                            credentialsId: 'Amazon Credentials',
                             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                         ]]) {
                             sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com"
