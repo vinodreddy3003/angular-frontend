e225b564188f27685d4b4e639f0f10732e443b6d
vinodreddy3003 <138984613+vinodreddy3003@users.noreply.github.com>
2024-04-25 19:25:38 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -12,7 +12,7 @@ def call(String imageName, String awsAccountId, String awsRegion) {
                             credentialsId: 'vinodh',
                             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                         ]]) {
-                            bat 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 810678507647.dkr.ecr.us-east-1.amazonaws.com'
+                            bat 'aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 810678507647.dkr.ecr.us-east-2.amazonaws.com'
                         }
                     }
                 }
@@ -33,16 +33,16 @@ def call(String imageName, String awsAccountId, String awsRegion) {
                     }
                 }
             }
-            stage('Pull and Run docker image') {
-                steps {
-                    script {
-                        bat 'docker pull ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:latest'
-                        bat 'docker run -d -p 4200:4200 ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:latest'
-                        bat 'docker ps'
-                        bat 'docker ps -a'
-                    }
-                }
-            }
+            // stage('Pull and Run docker image') {
+            //     steps {
+            //         script {
+            //             bat 'docker pull ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:latest'
+            //             bat 'docker run -d -p 4200:4200 ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:latest'
+            //             bat 'docker ps'
+            //             bat 'docker ps -a'
+            //         }
+            //     }
+            // }
         }
     }
 }
